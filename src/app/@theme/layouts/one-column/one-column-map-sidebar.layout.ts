c2da86118124bf00cac1fd5c30d69d3763317110
Evgeny Olshansky <e.olshansky@t-systems.com>
2023-07-30 09:24:09 +0300

    bugs

--- src/app/@theme/layouts/one-column/one-column-map-sidebar.layout.ts
+++ src/app/@theme/layouts/one-column/one-column-map-sidebar.layout.ts
@@ -9,21 +9,17 @@ import { Component } from '@angular/core';
         <travale-header></travale-header>
       </nb-layout-header>
 
-      <!--<nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive [compactedBreakpoints]="['xs', 'is', 'sm', 'md', 'lg']" [collapsedBreakpoints]="['is', 'xs', 'unknown']">
+      <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive [compactedBreakpoints]="['xs', 'is', 'sm', 'md', 'lg']" [collapsedBreakpoints]="['is', 'xs', 'unknown']">
         <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>-->
-
+      </nb-sidebar>
+      <nb-sidebar [collapsedBreakpoints]="['sm', 'md', 'lg', 'is', 'xs', 'unknown']" responsive class="map-sidebar"
+        tag="map-sidebar" #mapSidebar left="false" responsive>
+        <ng-content select="travale-leaflet-map-component"></ng-content>
+      </nb-sidebar>
       <nb-layout-column class="p-0 ps-3">
         <ng-content select="router-outlet"></ng-content>
       </nb-layout-column>
 
-      <!--<nb-sidebar [collapsedBreakpoints]="['sm', 'md', 'lg', 'is', 'xs', 'unknown']" responsive class="map-sidebar"
-        tag="map-sidebar" #mapSidebar left="false" responsive>
-        <ng-content select="travale-leaflet-map-component"></ng-content>
-      </nb-sidebar>-->
-      <nb-sidebar class="map-sidebar" tag="map-sidebar" left="false" responsive [compactedBreakpoints]="['xs', 'is', 'sm', 'md', 'lg']" [collapsedBreakpoints]="['is', 'xs', 'unknown']">
-        <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
 
       <nb-layout-footer fixed class="footer map-layout-footer">
         <travale-map-footer></travale-map-footer>
